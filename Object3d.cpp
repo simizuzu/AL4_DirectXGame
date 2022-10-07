f030371e7ab52310347fc983da32a6dbf0c47112
翔 清水 <k021g1379@g.neec.ac.jp>
2022-10-07 23:17:16 +0900

    資料10ページまで完成

--- Object3d.cpp
+++ Object3d.cpp
@@ -2,6 +2,12 @@
 #include <d3dcompiler.h>
 #include <DirectXTex.h>
 
+#include <fstream>
+#include <sstream>
+#include <string>
+#include <vector>
+using namespace std;
+
 #pragma comment(lib, "d3dcompiler.lib")
 
 using namespace DirectX;
@@ -389,6 +395,32 @@ void Object3d::LoadTexture()
 void Object3d::CreateModel()
 {
 	HRESULT result = S_FALSE;
+
+	// ファイルストリーム
+	std::ifstream file;
+	// .objファイルを開く
+	file.open("Resouces/triangle/triangle.obj");
+	// ファイルオープン失敗をチェック
+	if (file.fail())
+	{
+		assert(0);
+	}
+
+	vector<XMFLOAT3> positions;		// 頂点座標
+	vector<XMFLOAT3> normals;		// 法線ベクトル
+	vector<XMFLOAT2> texcoords;		// テクスチャUV
+	// 1行ずつ読み込む
+	string line;
+	while (getline(file, line))
+	{
+		// 1行分の文字列をストリームに変換して解析しやすくする
+		std::istringstream line_stream(line);
+
+		// 半角スペース区切りで行の先頭文字列を取得
+		string key;
+		getline(line_stream, key, ' ');
+	}
+	file.close();
 
 	std::vector<VertexPosNormalUv> realVertices;
 	// 頂点座標の計算（重複あり）
